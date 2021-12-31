51840b9f8b3fecd35befda3308ee780209c6c398
Дмитрий Владимирович <dlifenko@gmail.com>
2021-12-31 03:52:42 +0200

    add ScreenPosition

---
+++ Runtime/UI/ScreenPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Serenity.UI
+{
+	public class ScreenPosition
+	{
+		public Vector3 FromWorldPos(Vector3 worldPos) {
+			Vector3 screenPos = camera.WorldToScreenPoint(worldPos);
+			return screenPos;
+		}
+
+		public ScreenPosition(Camera camera) {
+			this.camera = camera;
+		}
+
+		private readonly Camera camera;
+	}
+}
